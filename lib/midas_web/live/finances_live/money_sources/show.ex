f8071cb118df0bc8fb46a36d073a6766fe837ba3
Pedro Soares <pedro.soareszl99@gmail.com>
2025-04-21 09:40:51 -0300

    created money sources list

--- lib/midas_web/live/finances_live/money_sources/show.ex
+++ lib/midas_web/live/finances_live/money_sources/show.ex
@@ -1,16 +1,33 @@
 defmodule MidasWeb.FinancesLive.MoneySources.Show do
   use MidasWeb, :live_view
+  import Phoenix.LiveView.JS
 
   alias Midas.Finances
+  alias Midas.Finances.MoneySource
 
   @impl true
   def mount(_params, _session, socket) do
-    money_sources = Finances.get_user_money_sources(socket.assigns.current_user.id)
+    money_sources = Finances.get_user_money_sources(socket.assigns.current_user)
 
     socket =
       socket
       |> assign(:money_sources, money_sources)
 
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    money_source = Finances.get_money_source!(id)
+
+    # Verificar se a conta pertence ao usuário atual
+    if money_source.user_id == socket.assigns.current_user.id do
+      {:ok, _} = Finances.delete_money_source(money_source)
+
+      money_sources = Finances.list_money_sources_by_user(socket.assigns.current_user.id)
+      {:noreply, assign(socket, :money_sources, money_sources)}
+    else
+      {:noreply, put_flash(socket, :error, "Você não tem permissão para excluir esta conta.")}
+    end
   end
 end
