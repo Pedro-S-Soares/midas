db19dbf8bc3500aded79a4c0544b04c8091260f6
Pedro Soares <pedro.soareszl99@gmail.com>
2025-04-21 09:34:56 -0300

    added tests to get_user_money_sources/1

--- test/midas/money_sources/search_test.exs
+++ test/midas/money_sources/search_test.exs
@@ -17,5 +17,32 @@ defmodule Midas.MoneySources.SearchTest do
       assert length(result) == 1
       assert result |> List.first() |> Map.get(:id) == money_source.id
     end
+
+    test "returns an empty list if the user has no money sources" do
+      # ARRANGE
+      user = Factory.insert(:user)
+
+      # ACT
+      result = Search.get_user_money_sources(user)
+
+      # ASSERT
+      assert length(result) == 0
+      assert result == []
+    end
+
+    test "returns only the user's money sources" do
+      # ARRANGE
+      user = Factory.insert(:user)
+      other_user = Factory.insert(:user)
+      _other_user_money_source = Factory.insert(:money_source, user: other_user)
+      user_money_source = Factory.insert(:money_source, user: user)
+
+      # ACT
+      result = Search.get_user_money_sources(user)
+
+      # ASSERT
+      assert length(result) == 1
+      assert result |> List.first() |> Map.get(:id) == user_money_source.id
+    end
   end
 end
